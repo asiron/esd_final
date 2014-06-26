fd697662b22888572da90914dcfda0af0355cdd7
Maciej <maciej.zurad@gmail.com>
2014-06-26 20:48:30 +0200

    Almost finishing .oil file

--- esd_final.c
+++ esd_final.c
@@ -6,51 +6,31 @@
 
 #define RIGHT_MOTOR_PORT NXT_PORT_A
 #define LEFT_MOTOR_PORT NXT_PORT_C
-#define TOUCH_SENSOR_PORT NXT_PORT_S4
-#define LIGHT_SENSOR_PORT NXT_PORT_S1
+#define SONAR_SENSOR_PORT NXT_PORT_S4
 
-#define MIN_SPEED 59
-#define MAX_SPEED 69
+#define MIN_SPEED 1
+#define MAX_SPEED 100
 
-#define EMERGENCY_SPEED 75
-
-#define GO_BACK_ROTATIONS 1.5
-#define GO_OUTSIDE_ROTATIONS 1.5
-#define TURN_DEGREES 200
 
 DeclareCounter(SysTimerCnt);
-DeclareEvent(TouchSensorOnEvent);
-DeclareEvent(LightSensorReadingEvent);
-DeclareEvent(EmergencyEvent);
-DeclareTask(LineFollowerTask);
-DeclareTask(EmergencyTask);
-DeclareTask(MotorControlTask);
-DeclareTask(TouchSensorTask);
-DeclareTask(LightSensorTask);
+
+DeclareAlarm(SonarSensorTaskTrigger);
+DeclareAlarm(MotorSensorTaskTrigger);
+DeclareAlarm(LCDTaskTrigger);
+
+DeclareTask(SonarSensorTask);
+DeclareTask(DataValidationTask);
+DeclareTask(DataProcessingTask);
+DeclareTask(OutputProcessingTask);
+DeclareTask(MonitorTask);
+DeclareTask(MotorSensorTask);
 DeclareTask(LCDTask);
 
 void user_1ms_isr_type2(void);
 void ecrobot_device_terminate(void);
 void ecrobot_device_initialize(void);
 void disp(int row, char* str, int val);
-void send_encoder_request();
 void send_motor_command(U32 motor, int speed, int brake);
-
-enum EMERGENCY_STATES {GO_BACK, NEEDS_TURN, TURNING, GO_AWAY}; 
-
-int touch_sensor = 0;
-int light_sensor = 0;
-
-int left_brake   = 0;
-int right_brake  = 0;
-
-int current_left_motor_speed  = 0;
-int current_right_motor_speed = 0;
-
-int initial_brightness = 0;
-
-int current_left_motor_count = 0;
-int current_right_motor_count = 0;
 
 /* nxtOSEK hook to be invoked from an ISR in category 2 */
 void user_1ms_isr_type2(void)
@@ -67,76 +47,28 @@ void user_1ms_isr_type2(void)
 
 void ecrobot_device_initialize(void)
 {
-	ecrobot_set_light_sensor_active(LIGHT_SENSOR_PORT);
+	ecrobot_init_sonar_sensor(SONAR_SENSOR_PORT);
 }
 
 void ecrobot_device_terminate(void)
 {
-	ecrobot_set_light_sensor_inactive(LIGHT_SENSOR_PORT);
+	ecrobot_term_sonar_sensor(SONAR_SENSOR_PORT);
 }
 
-TASK(LineFollowerTask)
+
+TASK(DataAcquisitionTask)
 {
-	StartCOM(COMAPP);
-
-	systick_wait_ms(300);
-
-	initial_brightness = light_sensor;
-	int new_brightness = initial_brightness;
-	int before_brightness = initial_brightness;
-
-	while(1) {
-
-		WaitEvent(LightSensorReadingEvent | EmergencyEvent);
-
-		EventMaskType mask = 0;
-		GetEvent(LineFollowerTask, &mask);
-
-		if (EmergencyEvent & mask) {
-			ClearEvent(EmergencyEvent);
-			TerminateTask();
-		} else if (LightSensorReadingEvent & mask) {
-			if ( new_brightness < (initial_brightness * 0.77) ) {
-
-				if ( before_brightness >= new_brightness ) {
-					// Turn to the right
-					send_motor_command(LEFT_MOTOR_PORT, MIN_SPEED, 0);
-					send_motor_command(RIGHT_MOTOR_PORT, MAX_SPEED, 0);
-				}
-				before_brightness = new_brightness;
-			} else {
-
-				// Turn to the left
-				send_motor_command(LEFT_MOTOR_PORT, MAX_SPEED, 0);
-				send_motor_command(RIGHT_MOTOR_PORT, MIN_SPEED, 0);
-			}
-			new_brightness = light_sensor;
-			ClearEvent(LightSensorReadingEvent);
-		}
-
+	if (!shutdown)
+	{
+		/* code */
 	}
 	TerminateTask();
 }
 
-TASK(TouchSensorTask)
+TASK(SonarSensorTask)
 {
-	static UINT8 old_touch_status = 0;
+	S32 sonar_sensor_value = ecrobot_get_sonar_sensor(LIGHT_SENSOR_PORT);
 
-	touch_sensor = ecrobot_get_touch_sensor(TOUCH_SENSOR_PORT);
-	
-	if (old_touch_status == 0 && touch_sensor == 1) {
-		SetEvent(EmergencyTask, TouchSensorOnEvent);
-	}
-
-	old_touch_status = touch_sensor;
-
-	TerminateTask();
-}
-
-TASK(LightSensorTask)
-{
-	light_sensor = ecrobot_get_light_sensor(LIGHT_SENSOR_PORT);
-	SetEvent(LineFollowerTask, LightSensorReadingEvent);
 	TerminateTask();
 }
 
--- msg_types.h
+++ msg_types.h
@@ -1,6 +1,21 @@
-typedef struct motor_ctr_msg_type_tag {
-	UINT8 motor;
-	INT8 speed;
-	INT8 brake;
-	UINT8 encoder_request;
-} MOTOR_CTR_MSG ;
+typedef struct sonar_sensor_reading_msg {
+	INT32 value;
+	UINT32 id;
+} SONAR_SENSOR_MSG ;
+
+typedef struct motor_command_msg {
+	UINT8 left_motor;
+	UINT8 right_motor;
+	UINT8 left_motor_break;
+	UINT8 right_motor_break;
+} MOTOR_COMMAND_MSG ;
+
+typedef struct data_validation_result_msg {
+	UINT8 result;
+	UINT32 id;
+} DATA_VALIDATION_RESULT_MSG ;
+
+typedef struct motor_sensor_msg {
+	int left_motor_count;
+	int right_motor_count;
+} MOTOR_SENSOR_MSG ;
